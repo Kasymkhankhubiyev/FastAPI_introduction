d152b09845cddbdc17b6a5d228e2490b4daea73c
Kasymkhan <kasymkhankhubievnis@gmail.com>
2023-08-09 22:18:04 +0500

    ended a chapter 2

--- model.py
+++ model.py
@@ -3,3 +3,23 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema"
+            }
+        }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
