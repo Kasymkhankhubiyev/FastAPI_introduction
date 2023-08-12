2e5e8789f7ae431804edb723cbed5d5b4c0dab4a
Kasymkhan <kasymkhankhubievnis@gmail.com>
2023-08-12 09:16:42 +0500

    started next chapter

--- todo.py
+++ todo.py
@@ -1,17 +1,17 @@
-from fastapi import APIRouter, Path
-from model import Todo, TodoItem
+from fastapi import APIRouter, Path, HTTPException, status
+from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
 todo_list = []
 
-@todo_router.post('/todo')
+@todo_router.post('/todo', status_code=201)
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {'message': 'Todo added successfully'}
 
 
-@todo_router.get('/todo')
+@todo_router.get('/todo', response_model=TodoItems)
 async def retrieve_todos() -> dict:
     return {'todos': todo_list}
 
@@ -21,9 +21,13 @@ async def get_single_todo(todo_id: int = Path(..., title='The ID of todo to retr
     for todo in todo_list:
         if todo.id ==todo_id:
             return {'todo': todo}
-        return {
-            "message": "Todo with inputed ID does not exist."
-        }
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Todo with the inputed ID does not exist."
+        )
+        # return {
+        #     "message": "Todo with inputed ID does not exist."
+        # }
     
 
 @todo_router.put("/todo/{todo_id}")
@@ -35,9 +39,13 @@ async def update_todo(todo_data: TodoItem, todo_id: int =
             return {
                 "message": "Todo update successfully."
             }
-        return {
-            "message": "Todo with inputed ID does not exist."
-        }
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Todo with inputed ID does not exist."
+        )
+        # return {
+        #     "message": "Todo with inputed ID does not exist."
+        # }
     
 
 @todo_router.delete('/todo/{todo_id}')
@@ -49,9 +57,10 @@ async def delete_single_todo(todo_id: int) -> dict:
             return {
                 "message": "Todo deleted successfully."
             }
-        return{
-            "message": "Todo with inputed ID does not exist."
-        }
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Todo with inputed ID does not exist."
+        )
     
 
 @todo_router.delete('/todo')
